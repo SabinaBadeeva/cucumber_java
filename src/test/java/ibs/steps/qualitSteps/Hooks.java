7a0d5c886a07e0cd01db3d7f7c4a4985e82cd927
SabinaBadeeva <sabina-piter@list.ru>
2024-04-20 17:45:29 +0300

    add change

--- src/test/java/ibs/steps/qualitSteps/Hooks.java
+++ src/test/java/ibs/steps/qualitSteps/Hooks.java
@@ -3,24 +3,41 @@ package ibs.steps.qualitSteps;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
+import java.util.Map;
 
 import static util.PopConst.*;
 
 public class Hooks {
-    public static ChromeDriver driver;
+    public static WebDriver driver;
 
-    @Before ()
+    @Before()
     @Step("Подключаемся к браузеру")
-    public void openPage() {
-        System.setProperty(PATH_CHROME_DRIVER_WINDOWS, CHROME_WINDOWS);
-        driver = new ChromeDriver();
+    public void openPage() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("browserVersion", "109.0");
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", false
+        ));
+
+        driver = new RemoteWebDriver(
+                URI.create("http://149.154.71.152:4444/wd/hub").toURL(),
+                capabilities
+        );
+
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get(URL);
+        driver.get("http://149.154.71.152:8080/");
     }
 
     @After()
